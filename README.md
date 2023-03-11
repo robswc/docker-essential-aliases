bd18383ca41888a835ff1c7c603d7dd5d9010e8e
robwsc <robert.s.w.carroll@gmail.com>
2023-03-11 14:33:02 -0500

    add commands

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ![docker-essential-aliases-logo](https://user-images.githubusercontent.com/38849824/222996047-9c454b3c-c8f5-4199-9f6d-0e24f5043ea6.png)
 
+# CURRENTLY IN TESTING
--- src/cmd/root.go
+++ src/cmd/root.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -9,21 +6,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "dea",
 	Short: "An application to make docker commands easier to use.",
-	Long: `An application to make docker commands easier to use.
+	Long: `An application to make docker commands easier to use by @robswc
 
-Github: https://github.com/robswc/docker-essential-aliases
+Docs & GitHub:
+https://github.com/robswc/docker-essential-aliases/wiki
+
 Twitter: https://twitter.com/robswc
 Email: robert.s.w.carroll@gmail.com
 
 `,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -32,13 +28,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.docker-essential-aliases.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
